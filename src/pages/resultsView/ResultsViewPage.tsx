6c4fb13db6e33a86ea26889cafbad6042f2fb3c2
🔧 Ino de Bruijn 🧬 <ino@ino.bio>
2021-12-01 17:32:00 -0500

    add ndex iframe with POST

--- src/pages/resultsView/ResultsViewPage.tsx
+++ src/pages/resultsView/ResultsViewPage.tsx
@@ -64,6 +64,7 @@ import UserMessager, {
 } from 'shared/components/userMessager/UserMessage';
 import { HelpWidget } from 'shared/components/HelpWidget/HelpWidget';
 import { buildCBioPortalPageUrl } from 'shared/api/urls';
+import IFrameLoader from 'shared/components/iframeLoader/IFrameLoader';
 
 export function initStore(
     appStore: AppStore,
@@ -415,6 +416,49 @@ export default class ResultsViewPage extends React.Component<
                                     store={store}
                                     appStore={this.props.appStore}
                                     urlWrapper={this.urlWrapper}
+                                />
+                            ) : (
+                                <LoadingIndicator
+                                    isLoading={true}
+                                    size={'big'}
+                                    center={true}
+                                />
+                            )}
+                        </MSKTab>
+                    );
+                },
+            },
+            {
+                id: ResultsViewTab.NDEX,
+                hide: () =>
+                    !getServerConfig().show_pathway_mapper ||
+                    !this.resultsViewPageStore.studies.isComplete ||
+                    !this.resultsViewPageStore.remoteNdexUrl.isComplete,
+                getTab: () => {
+                    const showPM =
+                        store.filteredSequencedSampleKeysByGene.isComplete &&
+                        store.oqlFilteredCaseAggregatedDataByOQLLine
+                            .isComplete &&
+                        store.genes.isComplete &&
+                        store.samples.isComplete &&
+                        store.patients.isComplete &&
+                        store.coverageInformation.isComplete &&
+                        store.filteredSequencedSampleKeysByGene.isComplete &&
+                        store.filteredSequencedPatientKeysByGene.isComplete &&
+                        store.selectedMolecularProfiles.isComplete &&
+                        store.oqlFilteredCaseAggregatedDataByUnflattenedOQLLine
+                            .isComplete;
+
+                    return (
+                        <MSKTab
+                            key={14}
+                            id={ResultsViewTab.NDEX}
+                            linkText={'NDEx'}
+                        >
+                            {showPM ? (
+                                <IFrameLoader
+                                    height={800}
+                                    url={`${this.resultsViewPageStore.remoteNdexUrl.result}`}
                                 />
                             ) : (
                                 <LoadingIndicator
